2372ee4fb477769f49f6e5ace30993ef496399dd
Ayush Singh <44469161+ayushsingh1601@users.noreply.github.com>
2021-12-30 01:39:35 +0530

    Update pollution_bot.py

--- pollution_bot.py
+++ pollution_bot.py
@@ -16,20 +16,12 @@ def my_listener(event):
     else:
         print('The job worked :)')
 
-def killpoll():
-  
-  while True:
-      time.sleep(45)
-      reddit = praw.Reddit(client_id='CLIENT_ID',
-               client_secret='CLIENT_SECRET',
-               user_agent='USER_AGENT',
-               username='airdelhi',
-               password='PASSWORD'
-           )
-      
-      with urllib.request.urlopen("https://api.data.gov.in/resource/3b01bcb8-0b14-4abf-b6f2-c1bfd384ba69?api-key=API_KEY&format=json&offset=191&limit=180") as url:
-          data = json.loads(url.read().decode())
-          
+
+def pollution():
+  try:
+      with urllib.request.urlopen("https://api.data.gov.in/resource/3b01bcb8-0b14-4abf-b6f2-c1bfd384ba69?api-key=API_KEY&format=json&offset=0&limit=500") as url:
+        data = json.loads(url.read().decode())
+              
       json_string = json.dumps(data)
       python_obj = json.loads(json_string)
 
@@ -46,6 +38,7 @@ def killpoll():
         "OZONE":-1
       }
       timeofupdate = ""
+      cou = 0
       for item in python_obj["records"]:
         station = item["station"]
         last_update = item["last_update"]
@@ -57,11 +50,22 @@ def killpoll():
           station = "North Campus, DU"
         if station == "Pus":
             station = "Pusa IMD"
+        if station == "Aya Naga":
+            station = "Aya Nagar"
+        if station == "IGI Airport (T3":
+            station = "IGI Airport (T3)"
+        if station == "CRRI Mathura Roa":
+            station = "CRRI Mathura Road"
 
         pollutant_name = item["pollutant_id"]
-        pollutant_min = item["pollutant_min"]
-        pollutant_max = item["pollutant_max"]
-        pollutant_avg = item["pollutant_avg"]
+        pollutant_min = "NA"
+        pollutant_max = "NA"
+        pollutant_avg = "NA"
+        if "pollutant_min" in item:
+            pollutant_min = item["pollutant_min"]
+            pollutant_max = item["pollutant_max"]
+            pollutant_avg = item["pollutant_avg"]
+
         city = item["city"]
         if(city == "Delhi"):
             new_dict = sample.copy()
@@ -72,11 +76,14 @@ def killpoll():
             else:
               pol_data[station][pollutant_name] = new_dict[pollutant_name]
         else :
-            print("Delhi Exceeded and others excluded")
-            break;
-
-      string = "**Area**^[AQIs-pollutant-wise]|**PM2.5**|**PM10**|**NO2**|**NH3**|**SO2**|**CO**|**OZONE**\n-|-|-|-|-|-|-|-\n"
-
+            cou = cou + 1
+    ##            print("Delhi Exceeded and others excluded")
+            
+      print("Not Delhi : ",cou)
+      string = "*DELHI POLLUTION*\n"
+      string += "\n"
+      string += "**Area**^[AQIs-pollutant-wise]|**PM2.5**|**PM10**|**NO2**|**NH3**|**SO2**|**CO**|**OZONE**\n-|-|-|-|-|-|-|-\n"
+      print(pol_data)
       for station in pol_data:
           if station == "Pusa IMD" or "Punjabi Bagh":
             string += station
@@ -93,55 +100,172 @@ def killpoll():
       string += "Last updated at: "
       string += timeofupdate
 
-      string += ".Data from -- Ministry of Environment and Forests , Central Pollution Control Board.\n"
-      string += ('^(Beep Boop! I am a Bot.Mention me in comments.)[^(My Creator)](https://www.reddit.com/message/compose/?to=Pollution_Hater).')
-      with urllib.request.urlopen("https://api.covid19india.org/v4/data.json") as url: #https://github.com/covid19india/api
-        cdata = json.loads(url.read().decode())
-      cjson_string = json.dumps(cdata)
-      covid_data = json.loads(cjson_string)
-      covid_string = "*DELHI COVID STATUS*\n"
-      covid_string += "\n"
-      covid_string += "**Confirmed**|**Deceased**|**Recovered**|**Tested**\n-|-|-|-\n"
-
-  
-
-      for key,value in covid_data["DL"]["total"].items():
-                  
-        if value == -1:
-          covid_string += "NA"
-        else:
-          covid_string += str(value)
-        covid_string += "|"
-      covid_string = covid_string[:-1]
-      covid_string += "\n"
-      covid_string += "-\n"
-      update = str(covid_data["DL"]["districts"]["Delhi"]["meta"]["tested"]["last_updated"])
-      covid_string += "Last updated at:"
-      covid_string += update
-      covid_string += "\n"
-      covid_string += ('^(Beep Boop! I am a Bot,Please be nice.Mention me in comments.)[^(My Creator)](https://www.reddit.com/message/compose/?to=Pollution_Hater).')
-
-      print("Checking for unread messages ")     
+      string += ".Data from -- Ministry of Environment and Forests , Central Pollution Control Board and covid19india.org.\n"
+      string += ('^(Beep Boop! I am a Bot.Mention me in comments.) [ ^(My Creator )](https://www.reddit.com/message/compose/?to=Pollution_Hater)')
+      return string
+  except Exception as e:
+      print("ERROR WITH POLLUTION API",e)
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+def covid():
+      try:
+          with urllib.request.urlopen("https://api.covid19tracker.in/data/static/data.min.json") as url:
+            cdata = json.loads(url.read().decode())
+          cjson_string = json.dumps(cdata)
+          covid_data = json.loads(cjson_string)
           
+          covid_string = "*DELHI COVID STATUS*\n"
+          covid_string += "\n"
+          covid_string += "**Confirmed**|**Deceased**|**Recovered**|**1st Dose**|**2nd Dose**|**Tested**\n-|-|-|-|-|-\n"
+          population = covid_data["DL"]["meta"]["population"]
+          update = str(covid_data["DL"]["meta"]["last_updated"])
+          for key,value in covid_data["DL"]["total"].items():
+                      
+            if value == -1:
+              covid_string += "NA"
+            elif key == "vaccinated1" or key == "vaccinated2":
+              vax = value/population
+              covid_string += str(vax)[2:4]
+              covid_string += "%"
+            elif value != 0:
+              covid_string += str(value)
+            covid_string += "|"
+          covid_string = covid_string[:-1]
+          covid_string += "\n"
+          covid_string += "\n"
+
+          covid_string += "Last updated at:"
+          covid_string += update[:-1]
+          covid_string += "\nFrom covid19tracker.in "
+          covid_string += ('^(Beep Boop! I am a Bot.Mention me in comments.) [ ^(My Creator )](https://www.reddit.com/message/compose/?to=Pollution_Hater)')
+          return covid_string
+      except Exception as e:
+          print("ERROR WITH COVID API",e)
+
+def noida_pollution():
+  try:
+      with urllib.request.urlopen("https://api.data.gov.in/resource/3b01bcb8-0b14-4abf-b6f2-c1bfd384ba69?api-key=API_KEY&format=json&offset=1400&limit=400") as url:
+        data = json.loads(url.read().decode())
+              
+      json_string = json.dumps(data)
+      python_obj = json.loads(json_string)
+
+      pol_data = {}
+
+      covid_string = ""
+      sample = {
+        "PM2.5":-1,
+        "PM10":-1,
+        "NO2":-1,
+        "NH3":-1,
+        "SO2":-1,
+        "CO":-1,
+        "OZONE":-1
+      }
+      timeofupdate = ""
+      cou = 0
+      for item in python_obj["records"]:
+        station = item["station"]
+        last_update = item["last_update"]
+        timeofupdate = last_update
+        station = station[:-14]
+        if "Greater" in station:
+          station = station[:-8]
+        if station != "Sector - 6":
+          station = station[:-1]
+
+        pollutant_name = item["pollutant_id"]
+        pollutant_min = "NA"
+        pollutant_max = "NA"
+        pollutant_avg = "NA"
+        if "pollutant_min" in item:
+            pollutant_min = item["pollutant_min"]
+            pollutant_max = item["pollutant_max"]
+            pollutant_avg = item["pollutant_avg"]
+
+        city = item["city"]
+        if(city == "Noida" or city == "Greater Noida"):
+            new_dict = sample.copy()
+            new_dict[pollutant_name] = pollutant_avg
+
+            if station not in pol_data.keys():
+              pol_data.update({station : new_dict})
+            else:
+              pol_data[station][pollutant_name] = new_dict[pollutant_name]
+        else :
+            cou = cou + 1
+    ##            print("Noida Exceeded and others excluded")
+            
+      print("Not Noida : ",cou)
+      string = "*NOIDA POLLUTION*\n"
+      string += "\n"
+      string += "**Area**^[AQIs-pollutant-wise]|**PM2.5**|**PM10**|**NO2**|**NH3**|**SO2**|**CO**|**OZONE**\n-|-|-|-|-|-|-|-\n"
+      print(pol_data)
+      for station in pol_data:
+          if station == "Pusa IMD" or "Punjabi Bagh":
+            string += station
+            string += "|"
+            for keys,value in pol_data[station].items():
+                if(value==-1):
+                    string += "NA"
+                else:
+                    string += str(value)
+                string += "|"
+            string = string[:-1]
+            string += "\n"
+      string += "-\n"
+      string += "Last updated at: "
+      string += timeofupdate
+
+      string += ".Data from -- Ministry of Environment and Forests , Central Pollution Control Board  and covid19india.org.\n"
+      string += ('^(Beep Boop! I am a Bot.Mention me in comments.) [ ^(My Creator )](https://www.reddit.com/message/compose/?to=Pollution_Hater)')
+      return string
+  except Exception as e:
+      print("ERROR WITH NOIDA POLLUTION API",e)
+    
+def killpoll():
+  
+  while True:
+      time.sleep(70)
+      reddit = praw.Reddit(client_id='CLIENT_ID',
+               client_secret='CLIENT_SECRET',
+               user_agent='airdelhi v1.0 by /u/Pollution_Hater',
+               username='airdelhi',
+               password='PASSWORD'
+           )
+      
+      string = pollution()
+      covid_string = covid()
+      noida_string = noida_pollution()
+      print("Checking for unread messages ")        
       for item in reddit.inbox.unread(limit=2):
-        
-        
-        if isinstance(item, Comment):
+       
+        if isinstance(item, Comment) or isinstance(item,Message):
           if hasattr(item,"body"):
             print(item.body)
             parent = item.parent()
             print("parent",parent)
             try:
-                print("FOUND unread MESSAGE !")
-              
+              print("FOUND unread MESSAGE !")
+              if(False):
+                print("No Ho Ho !")
+              else:
+                print("REPLYING !")
                 try:
                   item.mark_read()
-                  if "u/airdelhi" in item.body:
+                  item.body=item.body.lower()
+                  if "u/airdelhi" in item.body or "aqi" in item.body:
                     if "covid" in item.body or "corona" in item.body:
                         try:
                           item.reply(covid_string)
                         except Exception as er:
                           print("COVID EXCEPTION : %s"%(er))
+                          item.mark_unread()
+                    elif "noida" in item.body:
+                        try:
+                          item.reply(noida_string)
+                        except Exception as er:
+                          print("NOIDA COVID EXCEPTION : %s"%(er))
                           item.mark_unread()
                     else:
                         try:
@@ -150,7 +274,7 @@ def killpoll():
                         except Exception as e:
                           print("REPLY EXCEPTION:%s"%(e))
                           item.mark_unread()
-                  
+                          
                   else:
                       print("Not related comment")
                 except Exception as e:
@@ -164,7 +288,7 @@ def killpoll():
 try:   
   sched = BlockingScheduler()
   sched.add_listener(my_listener, EVENT_JOB_ERROR | EVENT_JOB_EXECUTED)
-  @sched.scheduled_job('interval', minutes=3)
+  @sched.scheduled_job('interval', minutes=5)
   def timed_job():
       print("Scheduler Start")
       time.sleep(5)
